6c74434c87c116c99fad7e6036b61b5c547e9445
kojix2 <2xijok@gmail.com>
2019-01-13 17:57:53 +0900

    v0.0.1

--- lib/rdataset.rb
+++ lib/rdataset.rb
@@ -1,12 +1,12 @@
 require "rdataset/version"
-require "rdataset/data"
 require "daru"
 
 module RDataset
   class << self
     def dataset(package_name, dataset_name)
-      p rdata_directory = File.expand_path('../data', __FILE__)
-      
+      rdata_directory = File.expand_path('../../data', __FILE__)
+      filepath = File.join(rdata_directory, package_name, dataset_name)
+      df = Daru::DataFrame.from_csv(filepath)
     end
   end
 end
