ad3d4326b925771968ba4666f9a143bb7658e870
kojix2 <2xijok@gmail.com>
2019-01-13 17:42:47 +0900

    remove data.rb

--- lib/rdataset.rb
+++ lib/rdataset.rb
@@ -3,4 +3,10 @@ require "rdataset/data"
 require "daru"
 
 module RDataset
+  class << self
+    def dataset(package_name, dataset_name)
+      p rdata_directory = File.expand_path('../data', __FILE__)
+      
+    end
+  end
 end
--- lib/rdataset/data.rb
+++
@@ -1,7 +0,0 @@
-module RDataset
-  class << self
-    def dataset(package_name, dataset_name)
-      p :test
-    end
-  end
-end
