b600ba828d770d52727287249ed1bf1c140b7f25
kojix2 <2xijok@gmail.com>
2019-01-14 23:45:21 +0900

    test wheager all csv files can be read

--- spec/daru_spec.rb
+++ spec/daru_spec.rb
@@ -2,4 +2,15 @@ RSpec.describe Daru::DataFrame do
   it 'can create dataframe from rdataset' do
     expect(Daru::DataFrame.from_rdataset('datasets', 'iris').class).to eq Daru::DataFrame
   end
+
+  rdata_directory = File.expand_path('../data', __dir__)
+  Dir.glob(File.join(rdata_directory, '/*')).each do |dirpath|
+    package = File.basename(dirpath)
+    Dir.glob(File.join(dirpath, '/*')).each do |filepath|
+      dataset = File.basename(filepath, '.csv')
+      it "can load the #{dataset} dataset" do
+        expect(Daru::DataFrame.from_rdataset(package, dataset).class).to eq Daru::DataFrame
+      end
+    end
+  end
 end
--- spec/rdataset_spec.rb
+++ spec/rdataset_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe RDataset do
   it 'can load the iris dataset' do
     expect(RDataset.load('datasets', 'iris').class).to eq Daru::DataFrame
   end
+
+  rdata_directory = File.expand_path('../data', __dir__)
+  Dir.glob(File.join(rdata_directory, '/*')).each do |dirpath|
+    package = File.basename(dirpath)
+    Dir.glob(File.join(dirpath, '/*')).each do |filepath|
+      dataset = File.basename(filepath, '.csv')
+      it "can load the #{dataset} dataset" do
+        expect(RDataset.load(package, dataset).class).to eq Daru::DataFrame
+      end
+    end
+  end
 end
