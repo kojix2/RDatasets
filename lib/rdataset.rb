1b52e94e1894b0ece836d2ae02ad8964e58511d5
kojix2 <2xijok@gmail.com>
2019-01-15 18:22:00 +0900

    use module_function

--- lib/rdataset.rb
+++ lib/rdataset.rb
@@ -2,17 +2,17 @@ require 'rdataset/version'
 require 'daru'
 
 module RDataset
-  class << self
-    def load(package_name, dataset_name)
-      file_path = filepath(package_name, dataset_name)
-      Daru::DataFrame.from_csv(file_path)
-    end
+  module_function
 
-    def filepath(package_name, dataset_name)
-      rdata_directory = File.expand_path('../data', __dir__)
-      dataset_name << '.csv'
-      File.join(rdata_directory, package_name, dataset_name)
-    end
+  def load(package_name, dataset_name)
+    file_path = filepath(package_name, dataset_name)
+    Daru::DataFrame.from_csv(file_path)
+  end
+
+  def filepath(package_name, dataset_name)
+    rdata_directory = File.expand_path('../data', __dir__)
+    dataset_name << '.csv'
+    File.join(rdata_directory, package_name, dataset_name)
   end
 end
 
