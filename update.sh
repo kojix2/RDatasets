777a6fb5f98c549a4be708aff339c529b6caf07d
kojix2 <2xijok@gmail.com>
2020-11-18 15:48:12 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 wget https://github.com/vincentarelbundock/Rdatasets/archive/master.zip
 unzip master.zip
 rm master.zip
-rsync -avr Rdatasets-master/csv/ data/
+rsync -avr --delete Rdatasets-master/csv/ data/
 cp -f Rdatasets-master/datasets.csv data/datasets.csv
 rm -rf Rdatasets-master
